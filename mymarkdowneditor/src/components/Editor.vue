f43c8ce423d18c659f38e556267f14d6c7a45dac
OrionB312 <yuta.git.orion@gmail.com>
2018-05-09 00:04:56 +0900

    markedインストール直後の状態

---
+++ mymarkdowneditor/src/components/Editor.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="editor">
+      <h1>エディター画面</h1>
+      <div class="editorWrapper">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'editor',
+  data(){
+      return{
+
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
