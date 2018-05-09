1df96d692d4e97d819f67388ff5fc0ae3bf5070c
OrionB312 <yuta.git.orion@gmail.com>
2018-05-09 23:38:18 +0900

    markedインストール後、シンプルなエディタ表示完成

--- mymarkdowneditor/src/components/Editor.vue
+++ mymarkdowneditor/src/components/Editor.vue
@@ -1,21 +1,40 @@
 <template>
   <div id="editor">
-      <h1>エディター画面</h1>
-      <div class="editorWrapper">
-      </div>
+    <h1>エディター画面</h1>
+    <div class="editorWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import marked from 'marked';
 export default {
   name: 'editor',
   data(){
       return{
-
+        markdown:'',
       }
+  },
+  methods:{
+    preview: function(){
+      return marked(this.markdown);
+    },
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.editorWrapper{
+  display: flex;
+}
+.markdown{
+  width:50%;
+  height:500px;
+}
+.preview{
+  width:50%;
+  text-align: left;
+}
 </style>
